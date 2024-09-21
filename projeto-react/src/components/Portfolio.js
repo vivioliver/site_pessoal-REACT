5b55cbe7c99177a0158947727c4805b0b2dd0b9a
vivioliver <122638547+vivioliver@users.noreply.github.com>
2024-09-21 15:14:50 -0300

    .

--- projeto-react/src/components/Portfolio.js
+++ projeto-react/src/components/Portfolio.js
@@ -3,7 +3,7 @@ import React from 'react';
 const projects = [
   { title: "AtemporalBrand", description: "O projeto consistiu em criar uma loja para realizar um projeto com a disciplina de Autoria Web e Banco de Dados. Criamos então uma loja de roupas fictícias (AtemporalBrand) que seria para atender os públicos feminino e masculino de todas as idades com um estilo de moda atemporal. ", tech: "Python, HTML, CSS, JavaScript", github: "https://github.com/jottav21/AtemporalBrand.git" },
   { title: "Jogo da memória", description: "Criação do jogo da menória para a disciplina de Programação para Internet.", tech: "JavaScript, HTML, CSS", github: "https://github.com/vivioliver/jogo_da_memoria.git" },
-  { title: "Site do professor", description: "Criação do site sobre o professor ylvester Stallone. Avaliação do 1° bimestre da matéria de autoria web.", tech: "HTML, CSS", github: "https://github.com/vivioliver/Site-do-professor-.git" }
+  { title: "Site do professor", description: "Criação do site sobre o professor Sylvester Stallone. Avaliação do 1° bimestre da matéria de autoria web.", tech: "HTML, CSS", github: "https://github.com/vivioliver/Site-do-professor-.git" }
 ];
 
 const Portfolio = () => {
